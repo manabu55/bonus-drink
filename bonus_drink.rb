9b426c781d92196d5d5e04bf6703774e6c74f71f
五十嵐学文 <manabu.ikarashi@gmail.com>
2016-07-14 18:18:39 +0900

    ドリンクのカウント処理を追加

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,17 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    current_bottles = amount  # 現在手元にあるドリンクの数
+    result = amount           # 手に入れたドリンクの数
+    while current_bottles >= 3
+      # 新しく手に入るドリンク数
+      bonus_bottles = current_bottles / 3
+      # 手元に残ったドリンクの数
+      current_bottles = bonus_bottles + (current_bottles - (bonus_bottles * 3))
+      result += bonus_bottles
+    end
+    return result
   end
 end
+
+# drink = BonusDrink.total_count_for(ARGV[0].to_i)
+# puts("total_drink=" + drink.to_s)
